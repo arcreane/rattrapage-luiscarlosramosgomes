c844b860eb16c9bf2d1c3536cf19682624f82a65
carlosgomes96 <luiscarlosramosgomes@gmail.com>
2022-09-11 18:53:17 +0200

    final commit

--- Document Scanner/Document Scanner/DocumentScanner.cpp
+++ Document Scanner/Document Scanner/DocumentScanner.cpp
@@ -6,7 +6,7 @@
 using namespace cv;
 using namespace std;
 
-///////////////  Project 2 - Document Scanner  //////////////////////
+// Document Scanner  //
 
 Mat imgOriginal, imgGray, imgBlur, imgCanny, imgThre, imgDil, imgErode, imgWarp, imgCrop;
 vector<Point> initialPoints, docPoints;
@@ -19,7 +19,7 @@ Mat preProcessing(Mat img)
 	Canny(imgBlur, imgCanny, 25, 75);
 	Mat kernel = getStructuringElement(MORPH_RECT, Size(3, 3));
 	dilate(imgCanny, imgDil, kernel);
-	//erode(imgDil, imgErode, kernel);
+	
 	return imgDil;
 }
 
@@ -29,7 +29,7 @@ vector<Point> getContours(Mat image) {
 	vector<Vec4i> hierarchy;
 
 	findContours(image, contours, hierarchy, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);
-	//drawContours(img, contours, -1, Scalar(255, 0, 255), 2);
+	
 	vector<vector<Point>> conPoly(contours.size());
 	vector<Rect> boundRect(contours.size());
 
@@ -39,7 +39,7 @@ vector<Point> getContours(Mat image) {
 	for (int i = 0; i < contours.size(); i++)
 	{
 		int area = contourArea(contours[i]);
-		//cout << area << endl;
+		
 
 		string objectType;
 
@@ -50,13 +50,12 @@ vector<Point> getContours(Mat image) {
 
 			if (area > maxArea && conPoly[i].size() == 4) {
 
-				//drawContours(imgOriginal, conPoly, i, Scalar(255, 0, 255), 5);
+				
 				biggest = { conPoly[i][0],conPoly[i][1] ,conPoly[i][2] ,conPoly[i][3] };
 				maxArea = area;
 			}
-			//drawContours(imgOriginal, conPoly, i, Scalar(255, 0, 255), 2);
-			//rectangle(imgOriginal, boundRect[i].tl(), boundRect[i].br(), Scalar(0, 255, 0), 5);
-		}
+			
+		} 
 	}
 	return biggest;
 }
@@ -81,7 +80,7 @@ vector<Point> reorder(vector<Point> points)
 		subPoints.push_back(points[i].x - points[i].y);
 	}
 
-	newPoints.push_back(points[min_element(sumPoints.begin(), sumPoints.end()) - sumPoints.begin()]); // 0
+	newPoints.push_back(points[min_element(sumPoints.begin(), sumPoints.end()) - sumPoints.begin()]); //0
 	newPoints.push_back(points[max_element(subPoints.begin(), subPoints.end()) - subPoints.begin()]); //1
 	newPoints.push_back(points[min_element(subPoints.begin(), subPoints.end()) - subPoints.begin()]); //2
 	newPoints.push_back(points[max_element(sumPoints.begin(), sumPoints.end()) - sumPoints.begin()]); //3
@@ -100,32 +99,31 @@ Mat getWarp(Mat img, vector<Point> points, float w, float h)
 	return imgWarp;
 }
 
+////CODE FOR IMAGES
 void main() {
 
-	string path = "Resources/paper.jpg";
+	string path = "Resources/paper3.jpg";
 	imgOriginal = imread(path);
-	//resize(imgOriginal, imgOriginal, Size(), 0.5, 0.5);
+	
 
-	// Preprpcessing - Step 1 
+	// Image processing
 	imgThre = preProcessing(imgOriginal);
 
-	// Get Contours - Biggest  - Step 2
+	// Get Contours
 	initialPoints = getContours(imgThre);
-	//drawPoints(initialPoints, Scalar(0, 0, 255));
+	//Reorder points
 	docPoints = reorder(initialPoints);
 	//drawPoints(docPoints, Scalar(0, 255, 0));
 
-	// Warp - Step 3 
+	// Warp 
 	imgWarp = getWarp(imgOriginal, docPoints, w, h);
 
-	//Crop - Step 4
+	//Crop 
 	int cropVal = 5;
 	Rect roi(cropVal, cropVal, w - (2 * cropVal), h - (2 * cropVal));
 	imgCrop = imgWarp(roi);
 
 	imshow("Image", imgOriginal);
-	//imshow("Image Dilation", imgThre);
-	//imshow("Image Warp", imgWarp);
 	imshow("Image Crop", imgCrop);
 
 	int k;
@@ -144,4 +142,39 @@ void main() {
 		
 }
 	}
+//DE FOR WEBCAM
+
+
+	//void main() {
+	//	VideoCapture cap(0);
+	//	
+
+	//	while (true) {
+
+	//		cap.read(imgOriginal);
+	//		// Preprocessing
+	//		imgThre = preProcessing(imgOriginal);
+
+	//		// Get Contours 
+	//		initialPoints = getContours(imgThre);
+
+	//		/////drawPoints(initialPoints, Scalar(0,0,255));
+	//		//docPoints = reorder(initialPoints);
+	//		////drawPoints(docPoints, Scalar(0, 255, 0));
+
+	//		//// Warp
+	//		//imgWarp = getWarp(imgOriginal, docPoints, w, h);
+
+	//		////Crop
+	//		//int cropVal = 5;
+	//		//Rect roi(cropVal, cropVal, w - (2 * cropVal), h - (2 * cropVal));
+	//		//imgCrop = imgWarp(roi);
+
+	//		imshow("Image", imgOriginal);
+	//		//show("Image Crop", imgCrop);
+	//		waitKey(1);
+	//	
+	//
+	
 }
+	
